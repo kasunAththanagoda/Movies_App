e87f8bf121d6d95afde0295388715a15d4f90597
kasun aththanagoda <kasunaththanagoda@gmail.com>
2023-10-10 20:14:24 +0530

    layout

--- movie-app-frontend/src/App.js
+++ movie-app-frontend/src/App.js
@@ -1,22 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
+  const [movies, setMovies] = useState();
+  // const [movie, setMovie] = useState();
+  // const [reviews, setReviews] = useState([]);
+
+
+
+  const getMovies = async () =>{
+    
+    try
+    {
+
+      const response = await api.get("api/v1/movies/getAllMovies");
+      console.log("data : ",response.data)
+      setMovies(response.data);
+
+    } 
+    catch(err)
+    {
+      console.log(err);
+    }
+  }
+
+
+
+  useEffect(() => {
+    getMovies();
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Routes>
+          <Route path="/" element={<Layout/>}>
+            {/* <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            <Route path="*" element = {<NotFound/>}></Route> */}
+          </Route>
+      </Routes>
       </header>
     </div>
   );
